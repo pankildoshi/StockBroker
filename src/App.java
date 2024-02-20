7eddb4238a1c63821021a12b407167a059e98524
pankildoshi <pankil19doshi@gmail.com>
2024-02-20 15:35:38 +0530

    Stock reading and updating complete

--- src/App.java
+++ src/App.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class App {
+import Models.Stock;
+import Services.StockUpdater;
+
+import java.util.List;
+
+public class App {
+    public static void main(String[] args){
+        System.out.println("Stock Broker Application Running...");
+
+        String filename = "data/stocks.txt";
+        List<Stock> stocks = Stock.readStocksFromFile(filename);
+
+        StockUpdater.start(stocks);
+
+    }
 }
